e243b91eb832b96666b61b92660c76dcb0a6748a
lauvel9 <lauravelasquez0806@gmail>
2024-07-29 16:44:59 -0500

    Models editado

--- Models/Autor.cs
+++ Models/Autor.cs
@@ -5,6 +5,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Servebooks.Models
 {
-    public int Id { get; set; }
-    public string Nombre { get; set; }
+    public class Autor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
 }
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -5,13 +5,16 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Servebooks.Models
 {
-    public int Id { get; set; }
-    public string Nombre { get; set; }
-    public string Apellido { get; set; }
-    public int DocumentoId { get; set; }
-    public string NumeroDeDocumento { get; set; }
-    public string Direccion { get; set; }
-    public string Telefono { get; set; }
-    public string Correo { get; set; }
-    public string Rol { get; set; }
-}
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int DocumentoId { get; set; }
+        public string NumeroDeDocumento { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Rol { get; set; }
+    }
+    }

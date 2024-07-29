fb02b2d0793afca621c8b97f762c04e81c8bf0c9
lauvel9 <lauravelasquez0806@gmail>
2024-07-29 16:58:53 -0500

    Models edit

---
+++ Services/Interface/ILibroRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Serve_books.Services.Interface
+{
+    public interface ILibroRepository
+    {
+        
+    }
+}
